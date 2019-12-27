9161b805993e9e59d652d4d46167093e6cfe5725
Hung Ly <hunghly@gmail.com>
2019-12-27 14:10:03 -0600

    worked on rendering jjj quiz

--- js/app.js
+++ js/app.js
@@ -2,18 +2,110 @@ import Question from "./Question.js";
 import Quiz from "./Quiz.js"
 
 const App = (() => {
-  // cache the DOM
-  const quizEl = document.querySelector(".jabquiz");
-  const quizQuestionEl = document.querySelector(".jabquiz__question");
-  const trackerEl = document.querySelector(".jabquiz__tracker");
-  const taglineEl = document.querySelector(".jabquiz__tagline");
-  const choicesEl = document.querySelector(".jabquiz__choices");
-  const progressInnerEl = document.querySelector(".progress_inner");
-  const nextButtonEl = document.querySelector(".next");
-  const restartButtonEl = document.querySelector(".restart");
+    // cache the DOM
+    const quizEl = document.querySelector(".jabquiz");
+    const quizQuestionEl = document.querySelector(".jabquiz__question");
+    const trackerEl = document.querySelector(".jabquiz__tracker");
+    const taglineEl = document.querySelector(".jabquiz__tagline");
+    const choicesEl = document.querySelector(".jabquiz__choices");
+    const progressInnerEl = document.querySelector(".progress__inner");
+    const nextButtonEl = document.querySelector(".next");
+    const restartButtonEl = document.querySelector(".restart");
 
+    const q1 = new Question("First President of the US?",
+        ["Barrack", "Osama", "George", "Monkey"],
+        2
+    );
 
-  
+    const q2 = new Question("When was Javascript created?",
+        ["June 1995", "May 1995", "July 1885", "Sep 1996"],
+        1
+    );
+
+    const q3 = new Question("What does CSS stand for?",
+        ["County Sheriff Service", "Cascading sexy sheets", "cascading style sheets"],
+        2
+    );
+
+    const q4 = new Question("The full form of HTML is...?",
+        ["Hyper Text Markup Language", "Hold the Mic", "Error"],
+        0
+    );
+
+    const q5 = new Question("console.log(typeof []) returns what?",
+        ["array", "string", "Object", "undefined"],
+        2
+    );
+
+    const quiz = new Quiz([q1, q2, q3, q4, q5]);
+
+    const setValue = (elem, value) => {
+        elem.innerHTML = value;
+    };
+
+    const renderQuestion = _ => {
+        const question = quiz.getCurrentQuestion().question;
+        setValue(quizQuestionEl, question);
+    };
+
+    const renderChoicesElements = _ => {
+        let markup = "";
+        const currentChoices = quiz.getCurrentQuestion().choices;
+        currentChoices.forEach((elem, index) => {
+            markup += ` <li class="jabquiz__choice">
+          <input type="radio" name="choice" class="jabquiz__input" data-order="${index}" id="choice${index}">
+          <label for="choice${index}" class="jabquiz__label">
+            <i></i>
+            <span>${elem}</span>
+          </label>
+        </li>`
+        });
+        choicesEl.innerHTML = markup;
+    };
+
+    const renderTracker = _ => {
+        const index = quiz.currentIndex;
+        setValue(trackerEl, `${index + 1} of ${quiz.questions.length}`)
+    };
+
+    const getPercentage = (num1, num2) => {
+        return Math.round((num1 / num2) * 100);
+    };
+
+    const renderProgress = _ => {
+        // 1. width
+        const currentWidth = getPercentage(quiz.currentIndex, quiz.questions.length);
+        // 2. create function that goes from 0 to width
+        launch(0, currentWidth);
+    };
+    renderProgress();
+
+    const launch = (width, maxPercent) => {
+        let loadingBar = setInterval(function () {
+            if (width > maxPercent) {
+                clearInterval(loadingBar);
+            } else {
+                width++;
+                progressInnerEl.style.width = width + "%";
+            }
+        }, 3);
+    };
+
+    const renderAll = _ => {
+        if (quiz.hasEnded()) {
+            //render end screen
+        } else {
+            // 1. render the question
+            // 2. render the choices elements
+            // 3. render the tracker
+            // 4. render progress
+            renderQuestion();
+            renderChoicesElements();
+            renderTracker();
+            renderProgress();
+        }
+    };
+    renderAll();
 })();
 
 /*
